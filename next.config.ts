57aa7101b6bbb8d1c91bcdcffa9b341824787d6e
masterly14 <svaron066@gmail.com>
2025-08-25 09:00:30 -0500

    first_commit

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ['assets.aceternity.com'],
-  }
+  },
+  eslint: {
+    // Desactiva la comprobación de ESLint durante la compilación de Next.js
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
